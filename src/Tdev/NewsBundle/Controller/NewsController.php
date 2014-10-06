05f76199ac0fc02a1dae103c1dac2b8986fe4004
project-tdev <development@tdev.be>
2014-10-06 16:09:43 +0200

    First bundle : News

---
+++ src/Tdev/NewsBundle/Controller/NewsController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tdev\NewsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class NewsController extends Controller
+{
+    public function indexAction($page)
+    {
+        if($page<1)
+            throw new NotFoundHttpException('Les pages commencent à 1');
+        return $this->render('TdevNewsBundle:News:index.html.twig');
+    }
+    public function viewAction($id)
+    {
+        return $this->render('TdevNewsBundle:News:view.html.twig', array('id' => $id));
+    }
+    public function addAction()
+    {
+        $request = $this->getRequest();
+        if($request->isMethod('POST')){
+            $request->getSession()->getFlashBag()->add('success','News ajoutée');
+            return $this->redirect($this->generateUrl("tdev_news_view",array('id'=>1)));
+        }
+        return $this->render('TdevNewsBundle:News:add.html.twig');
+    }
+    public function editAction($id)
+    {
+        $request = $this->getRequest();
+        if($request->isMethod('POST')){
+            $request->getSession()->getFlashBag()->add('success',"News $id editée");
+            return $this->redirect($this->generateUrl("tdev_news_homepage"));
+        }
+        return $this->render('TdevNewsBundle:News:edit.html.twig', array('name' => $id));
+    }
+    public function deleteAction($id)
+    {
+        $request = $this->getRequest();
+        if($request->isMethod('POST')){
+            $request->getSession()->getFlashBag()->add('success',"News $id supprimée");
+            return $this->redirect($this->generateUrl("tdev_news_homepage"));
+        }
+        return $this->render('TdevNewsBundle:News:delete.html.twig', array('id' => $id));
+    }
+}
---
+++ src/Tdev/NewsBundle/TdevNewsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tdev\NewsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TdevNewsBundle extends Bundle
+{
+}
